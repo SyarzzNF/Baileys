8739127ee4018012cd4ad5fffada93cfc77448a2
Syarzz Hanami <144473220+SyarzzNF@users.noreply.github.com>
2025-03-28 10:32:03 +0700

    Update generics.ts

--- src/Utils/generics.ts
+++ src/Utils/generics.ts
@@ -206,11 +206,11 @@ export const generateMessageIDV2 = (userId?: string): string => {
 	random.copy(data, 28)
 
 	const hash = createHash('sha256').update(data).digest()
-	return 'B1EY' + hash.toString('hex').toUpperCase().substring(0, 16)
+	return 'SHIZUNO' + hash.toString('hex').toUpperCase().substring(0, 18)
 }
 
 // generate a random ID to attach to a message
-export const generateMessageID = () => 'B1EY' + randomBytes(8).toString('hex').toUpperCase()
+export const generateMessageID = () => 'SHIZUNO' + randomBytes(7).toString('hex').toUpperCase()
 
 export function bindWaitForEvent<T extends keyof BaileysEventMap>(ev: BaileysEventEmitter, event: T) {
 	return async(check: (u: BaileysEventMap[T]) => Promise<boolean | undefined>, timeoutMs?: number) => {
